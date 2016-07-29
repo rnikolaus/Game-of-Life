bd219fe287cf1a1dc664cf7df7d482121b59062f
rnikolaus <rnikolaus@users.noreply.github.com>
2016-07-29 16:56:18 +0200

    minor change

--- src/test/java/rnikolaus/gameoflife/GameOfLifeTest.java
+++ src/test/java/rnikolaus/gameoflife/GameOfLifeTest.java
@@ -1,17 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package rnikolaus.gameoflife;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import javax.naming.spi.DirStateFactory;
-import org.junit.Test;
-import static org.junit.Assert.*;
+package rnikolaus.gameoflife;
 
 /**
  *
